f22acd7ddf5722ff0f5d839228500e8b8fb6bea6
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-13 12:35:47 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * An example of methods
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+  // takes the width and length to calculate the area of a rectangle
+  public static double areaRectangle(double width, double length) {
+    double area = width * length;
+    return area;
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // ask what measurements they want
+    System.out.println("What length do you want?");
+    double width = input.nextDouble();
+    System.out.println("What width do you want?");
+    double length = input.nextDouble();
+
+    // get the answer
+    double answer = areaRectangle(width, length);
+    System.out.println("The area of your rectangle is " + answer);
+    
     
   }
 }
